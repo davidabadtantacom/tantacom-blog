5f7b3019267978ba853b2e6480f30551aae19fee
Israel Martín García <israel.martin.g@gmail.com>
2016-11-06 20:53:25 +0100

    Cambia short tags

--- wp-content/themes/tanta/header.php
+++ wp-content/themes/tanta/header.php
@@ -14,40 +14,40 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="google-site-verification" content="xTCxX19YjdDm59dnuT3xLykCyJFnmzXu6g1aeOmYDLE" />
 <title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
-	global $page, $paged;
+    /*
+     * Print the <title> tag based on what is being viewed.
+     */
+    global $page, $paged;
 
-	wp_title( '|', true, 'right' );
+    wp_title( '|', true, 'right' );
 
-	// Add the blog name.
-	bloginfo( 'name' );
+    // Add the blog name.
+    bloginfo( 'name' );
 
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
+    // Add the blog description for the home/front page.
+    $site_description = get_bloginfo( 'description', 'display' );
+    if ( $site_description && ( is_home() || is_front_page() ) )
+        echo " | $site_description";
 
 
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'tanta' ), max( $paged, $page ) );
+    // Add a page number if necessary:
+    if ( $paged >= 2 || $page >= 2 )
+        echo ' | ' . sprintf( __( 'Page %s', 'tanta' ), max( $paged, $page ) );
 
-	?></title>
+    ?></title>
 
-	<?
-	if ( is_category()  ){
-		global $post;
-		$categories = get_the_category($post->ID);
-	 	$des =  "Publicamos artículos en profundidad sobre ".$categories[0]->cat_name."...Consúltalos" ?>
-	 	<meta name="description" content="<? echo $des; ?>" />
-	<?
-	}else if(is_date()){ ?>
+    <?php
+    if ( is_category()  ){
+        global $post;
+        $categories = get_the_category($post->ID);
+        $des =  "Publicamos artículos en profundidad sobre ".$categories[0]->cat_name."...Consúltalos"; ?>
+        <meta name="description" content="<? echo $des; ?>" />
+    <?php
+    }else if(is_date()){ ?>
 
-	 	<meta name="description" content="Publicamos artículos en profundidad sobre temas de actualidad Internet y entornos móviles: Usabilidad, creatividad, desarrollo frontend y accesibilidad web, desarrollo backend y cms, promoción on y offpage, tendencias... Consúltalos" />
+        <meta name="description" content="Publicamos artículos en profundidad sobre temas de actualidad Internet y entornos móviles: Usabilidad, creatividad, desarrollo frontend y accesibilidad web, desarrollo backend y cms, promoción on y offpage, tendencias... Consúltalos" />
 
-	<? } ?>
+    <?php } ?>
 
 
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
@@ -66,18 +66,18 @@
 
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php
-	/* We add some JavaScript to pages with the comment form
-	 * to support sites with threaded comments (when in use).
-	 */
-	if ( is_singular() && get_option( 'thread_comments' ) )
-		wp_enqueue_script( 'comment-reply' );
+    /* We add some JavaScript to pages with the comment form
+     * to support sites with threaded comments (when in use).
+     */
+    if ( is_singular() && get_option( 'thread_comments' ) )
+        wp_enqueue_script( 'comment-reply' );
 
-	/* Always have wp_head() just before the closing </head>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to add elements to <head> such
-	 * as styles, scripts, and meta tags.
-	 */
-	wp_head();
+    /* Always have wp_head() just before the closing </head>
+     * tag of your theme, or you will break many plugins, which
+     * generally use this hook to add elements to <head> such
+     * as styles, scripts, and meta tags.
+     */
+    wp_head();
 ?>
 
 
@@ -85,19 +85,19 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<div id="wrapper">
-		<aside>
-			<nav>
-			<header>
-				<a href="http://www.tantacom.com"><img src="<?php bloginfo( 'template_directory' ); ?>/images/logo.png" height="39px" width="170px" alt="Tanta" id="logo" /></a>
-			</header>
-				<ul>
-					<li><a href="http://www.tantacom.com/quienes-somos">quiénes somos</a></li>
-					<li><a href="http://www.tantacom.com/soluciones">soluciones</a></li>
-					<li><a href="http://www.tantacom.com/clientes">casos de éxito</a></li>
-					<li><a class="sel" href="http://www.tantacom.com/el-blog-de-tanta">blog</a></li>
-					<li><a href="http://www.tantacom.com/ofertas-de-empleo">únete</a></li>
-					<li><a href="http://www.tantacom.com/contacto">contacto</a></li>
-				</ul>
-			</nav>
-		</aside>
+    <div id="wrapper">
+        <aside>
+            <nav>
+            <header>
+                <a href="http://www.tantacom.com"><img src="<?php bloginfo( 'template_directory' ); ?>/images/logo.png" height="39px" width="170px" alt="Tanta" id="logo" /></a>
+            </header>
+                <ul>
+                    <li><a href="http://www.tantacom.com/quienes-somos">quiénes somos</a></li>
+                    <li><a href="http://www.tantacom.com/soluciones">soluciones</a></li>
+                    <li><a href="http://www.tantacom.com/clientes">casos de éxito</a></li>
+                    <li><a class="sel" href="http://www.tantacom.com/el-blog-de-tanta">blog</a></li>
+                    <li><a href="http://www.tantacom.com/ofertas-de-empleo">únete</a></li>
+                    <li><a href="http://www.tantacom.com/contacto">contacto</a></li>
+                </ul>
+            </nav>
+        </aside>
