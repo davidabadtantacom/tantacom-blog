7a413b332acd548a7e2105e34b8f2eba8b03fab7
David Abad Quijada <david.abad@tantacom.com>
2017-02-01 18:03:28 +0100

    Personalizacion de la pagina not found

--- wp-content/themes/foundry-child/js/tanta.js
+++ wp-content/themes/foundry-child/js/tanta.js
@@ -1,3 +1,8 @@
 // Parche para el comportamiento del menu en Firefox al abrir la portada con un ancla en la URL
 if (jQuery.browser.mozilla && location.hash) {setTimeout(function() {window.scrollTo(0, 0);}, 1);}
-jQuery(document).ready(function(){if (jQuery.browser.mozilla && window.location.hash) {var anchor = jQuery(window.location.hash);if (anchor.length){jQuery('body .nav-container nav:first').addClass('fixed');setTimeout(function() {jQuery('html, body').animate({scrollTop:anchor.offset().top});}, 100);}}});
+jQuery(document).ready(function(){
+	// Parche para el comportamiento del menu en Firefox al abrir la portada con un ancla en la URL
+	if (jQuery.browser.mozilla && window.location.hash) {var anchor = jQuery(window.location.hash);if (anchor.length){jQuery('body .nav-container nav:first').addClass('fixed');setTimeout(function() {jQuery('html, body').animate({scrollTop:anchor.offset().top});}, 100);}}
+	// Animacion para la opacidad de la bombilla en la pagina de contenido no encontrado
+	if (jQuery('.notFoundZone').length){jQuery('.notFoundZone').find('.background-image-holder').animate ({opacity: 1}, {duration: 1000, queue: false});jQuery('.notFoundZone').find('.container').animate ({opacity: 1}, {duration: 5000, queue: true});}
+});
