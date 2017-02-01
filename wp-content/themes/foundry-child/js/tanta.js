62cc79a18a60a22861d53ae20279bd4d6a5df2c4
David Abad Quijada <david.abad@tantacom.com>
2017-02-01 19:09:59 +0100

    Mejora visual de la pagina not found

--- wp-content/themes/foundry-child/js/tanta.js
+++ wp-content/themes/foundry-child/js/tanta.js
@@ -4,5 +4,5 @@ jQuery(document).ready(function(){
 	// Parche para el comportamiento del menu en Firefox al abrir la portada con un ancla en la URL
 	if (jQuery.browser.mozilla && window.location.hash) {var anchor = jQuery(window.location.hash);if (anchor.length){jQuery('body .nav-container nav:first').addClass('fixed');setTimeout(function() {jQuery('html, body').animate({scrollTop:anchor.offset().top});}, 100);}}
 	// Animacion para la opacidad de la bombilla en la pagina de contenido no encontrado
-	if (jQuery('.notFoundZone').length){jQuery('.notFoundZone').find('.background-image-holder').animate ({opacity: 1}, {duration: 1000, queue: false});jQuery('.notFoundZone').find('.container').animate ({opacity: 1}, {duration: 5000, queue: true});}
+	if (jQuery('.notFoundZone').length){jQuery('.notFoundZone').find('.imagen').animate ({opacity: 1}, {duration: 1000, queue: false});jQuery('.notFoundZone').find('.texto').animate ({opacity: 1}, {duration: 5000, queue: true});}
 });
