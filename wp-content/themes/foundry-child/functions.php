33bc3a463489c31fa6be9f1512531a24aadce421
David Abad Quijada <david.abad@tantacom.com>
2017-02-02 15:55:20 +0100

    Mejora visual del formulario de contacto. Se elimina el css personalizado, que será incluído desde el backend

--- wp-content/themes/foundry-child/functions.php
+++ wp-content/themes/foundry-child/functions.php
@@ -1,7 +1,6 @@
 <?php 
 
 function prefix_add_footer_styles_scripts() {
-    wp_enqueue_style( 'tanta-css', '/wp-content/themes/foundry-child/css/tanta.css' );
 	wp_enqueue_script( 'tanta-js', '/wp-content/themes/foundry-child/js/tanta.js' );
 };
 add_action( 'get_footer', 'prefix_add_footer_styles_scripts' );
