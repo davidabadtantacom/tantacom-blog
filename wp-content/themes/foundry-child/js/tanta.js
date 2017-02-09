cd2d51e2df07a2faf6444129e83125889dd0d22b
David Abad Quijada <david.abad@tantacom.com>
2017-02-09 12:24:57 +0100

    Eliminado plugin jquery-smooth-scroll (se pasa su funcionalidad a tanta.js), correccion posicionamiento scroll al usar las anclas del menu principal para mobile y visualizacion del menu siempre en mobile

--- wp-content/themes/foundry-child/js/tanta.js
+++ wp-content/themes/foundry-child/js/tanta.js
@@ -7,4 +7,21 @@ jQuery(document).ready(function(){
 	if (jQuery.browser.mozilla && window.location.hash) {var anchor = jQuery(window.location.hash);if (anchor.length){jQuery('body .nav-container nav:first').addClass('fixed');setTimeout(function() {jQuery('html, body').animate({scrollTop:anchor.offset().top});}, 100);}}
 	// Animacion para la opacidad de la bombilla en la pagina de contenido no encontrado
 	var redireccionNotFound;function luzEncendida () {redireccionNotFound = setTimeout(function(){ location.href = '/'; }, 3000);}function luzApagada () {clearTimeout(redireccionNotFound);}jQuery('.notFoundZone').on ('click', '.interruptor', function (){if (jQuery(this).attr('src').filename() == 'interruptor-off'){jQuery(this).attr('src', jQuery(this).attr('src').replace('interruptor-off', 'interruptor-on'));jQuery('.notFoundZone').find('.background-image-holder').css ('background-image', jQuery('.notFoundZone').find('.background-image-holder').css ('background-image').replace ('lamp-off', 'lamp-on'));luzEncendida ();}else if (jQuery(this).attr('src').filename() == 'interruptor-on'){jQuery(this).attr('src', jQuery(this).attr('src').replace('interruptor-on', 'interruptor-off'));jQuery('.notFoundZone').find('.background-image-holder').css ('background-image', jQuery('.notFoundZone').find('.background-image-holder').css ('background-image').replace ('lamp-on', 'lamp-off'));luzApagada ();}});
+	// Parche para hacer la navegacion mediante anclas más atractiva
+	jQuery('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = jQuery(this.hash);
+			target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				// Realizamos la accion del boton del menu
+				if (jQuery (this).parents ('.menu').length){
+					jQuery('.mobile-toggle').trigger ('click');
+				}
+				jQuery('html, body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
 });
