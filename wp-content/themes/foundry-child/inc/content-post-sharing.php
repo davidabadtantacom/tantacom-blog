12183a2cd8152dd450472a8de38e6adeb98998dd
David Abad Quijada <david.abad@tantacom.com>
2017-02-15 08:50:16 +0100

    Mejoras accesibilidad

--- wp-content/themes/foundry-child/inc/content-post-sharing.php
+++ wp-content/themes/foundry-child/inc/content-post-sharing.php
@@ -8,17 +8,17 @@
 <ul class="tags text-right sharePost">
     <li>
         <a class="btn btn-sm btn-icon" target="_blank" href="https://facebook.com/sharer.php?u=<?php the_permalink(); ?>" onClick="return ebor_facebook_<?php echo the_ID(); ?>()">
-            <i class="ti-facebook"></i>
+            <i class="ti-facebook"><span class="hidden">Facebook</span></i>
         </a>
     </li>
     <li>
         <a class="btn btn-sm btn-icon" target="_blank" href="https://twitter.com/share?original_referer=<?php the_permalink(); ?>&url=<?php echo wp_get_shortlink(); ?>&text=<?php echo get_the_title(); ?>&via=tantacom" onClick="return ebor_tweet_<?php echo the_ID(); ?>()">
-            <i class="ti-twitter-alt"></i>
+            <i class="ti-twitter-alt"><span class="hidden">Twitter</span></i>
         </a>
     </li>
     <li>
         <a class="btn btn-sm btn-icon" target="_blank" href="http://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>&title=<?php echo get_the_title(); ?>&source=<?php echo get_site_url(); ?>" onClick="return ebor_linkedin_<?php echo the_ID(); ?>()">
-            <i class="ti-linkedin-alt"></i>
+            <i class="ti-linkedin-alt"><span class="hidden">Linkedin</span></i>
         </a>
     </li>
 </ul>
