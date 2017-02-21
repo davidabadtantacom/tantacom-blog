620be92b706832c42052965486309ce117d4168d
David Abad Quijada <david.abad@tantacom.com>
2017-02-21 11:16:14 +0100

    Compartir posts desde la vista individual de estos

--- wp-content/themes/foundry-child/inc/content-post-sharing.php
+++ wp-content/themes/foundry-child/inc/content-post-sharing.php
@@ -1,11 +1,14 @@
 <?php
-	global $post;
-	
-	$url[] = '';
-	$url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full');
+    global $post;
+    
+    $url[] = '';
+    $url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full');
 ?>
 
 <ul class="tags text-right sharePost">
+    <li class="texto hidden">
+        <h6 class="uppercase"><?php _e('Share The Love','foundry'); ?></h6>
+    </li>
     <li>
         <a class="btn btn-sm btn-icon" target="_blank" href="https://facebook.com/sharer.php?u=<?php the_permalink(); ?>" onClick="return ebor_facebook_<?php echo the_ID(); ?>()">
             <span class="iconized ti-facebook"><span class="text">Facebook</span></span>
@@ -28,12 +31,12 @@
         window.open('https://facebook.com/sharer.php?u=<?php the_permalink(); ?>','sharer','toolbar=0,status=0,width=626,height=436');
         return false;
     }
-	function ebor_tweet_<?php echo the_ID(); ?>() {
-		window.open('https://twitter.com/share?original_referer=<?php the_permalink(); ?>&url=<?php echo wp_get_shortlink(); ?>&text=<?php echo get_the_title(); ?>&via=tantacom','sharer','toolbar=0,status=0,width=626,height=436');
-		return false;
-	}
-	function ebor_linkedin_<?php echo the_ID(); ?>() {
-		window.open('http://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>&title=<?php echo get_the_title(); ?>&source=<?php echo get_site_url(); ?>','sharer','toolbar=0,status=0,width=626,height=436');
-		return false;
-	}
+    function ebor_tweet_<?php echo the_ID(); ?>() {
+        window.open('https://twitter.com/share?original_referer=<?php the_permalink(); ?>&url=<?php echo wp_get_shortlink(); ?>&text=<?php echo get_the_title(); ?>&via=tantacom','sharer','toolbar=0,status=0,width=626,height=436');
+        return false;
+    }
+    function ebor_linkedin_<?php echo the_ID(); ?>() {
+        window.open('http://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>&title=<?php echo get_the_title(); ?>&source=<?php echo get_site_url(); ?>','sharer','toolbar=0,status=0,width=626,height=436');
+        return false;
+    }
 </script>
